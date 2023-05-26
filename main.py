d681b97900c6dc1547878e708949c7eaf5b85dc5
Gabriel Moreira C <128189786+gmoreirac26@users.noreply.github.com>
2023-05-26 15:26:28 -0300

    OK
    Signed-off-by: Gabriel Moreira C <128189786+gmoreirac26@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,4 +3,7 @@
 
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
-    return False
+    cleaned_string = ''.join(char.lower() for char in string if char.isalnum())
+    reversed_string = cleaned_string[::-1]
+    return cleaned_string == reversed_string
+   
